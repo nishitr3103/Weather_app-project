63761a26d088385e085993a337e7bda89c1b0672
Nishit R <nishitr3103@gmail.com>
2021-10-05 17:32:36 +0530

    logic to print invalid location

--- public/js/weatherpage.js
+++ public/js/weatherpage.js
@@ -11,7 +11,10 @@ form.addEventListener('submit',(e)=>{
     //console.log(location);
     fetch('http://api.weatherstack.com/current?access_key=7c5815294d4b4873635a53e7fcda49cd&query='+encodeURIComponent(location)).then((res)=>{
     res.json().then((data)=>{
-        if(data.error){console.log('Enter correct location')}
+        if(data.error){console.log('Enter correct location')
+            msg1.textContent="Invalid location !!!"
+            msg2.textContent="Please enter correct address";    
+        }
         else{
             console.log(data.location.name+', '+data.location.region+", " +data.location.country
             + '\n' +' '+data.current.weather_descriptions[0] +' '+ data.current.temperature+' '+data.current.weather_icons[0]);
